873057b52cf9ee69fc17e192a4cc67515fc76f42
oborogri <grigore.oboroceanu@gmail.com>
2016-09-23 12:07:57 +0100

    MyRent-00(step02-07): Residence model implemented

--- app/src/main/java/org/wit/myrent/MyRentActivity.java
+++ app/src/main/java/org/wit/myrent/MyRentActivity.java
@@ -2,6 +2,8 @@ package org.wit.myrent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MyRentActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MyRentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myrent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate the menu; this adds item to the action bar if it is present
+        getMenuInflater().inflate(R.menu.myrent, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //handle action bar items clicks here. The action bar will
+        //automatically handle clicks on the Home/Up button, so long
+        //as you specify a parent activity in AndroidManifest.xml
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/org/wit/myrent/Residence.java
@@ -0,0 +1,27 @@
+package org.wit.myrent;
+
+import java.util.Random;
+
+public class Residence
+{
+    private Long id;
+
+    //a latitude longitude pair
+    //example "52.4566,-6.5444"
+    private String geolocation;
+
+    public Residence()
+    {
+        id = new Random().nextLong();
+    }
+
+    public void setGeolocation(String geolocation)
+    {
+        this.geolocation = geolocation;
+    }
+
+    public String getGeolocation()
+    {
+        return geolocation;
+    }
+}
