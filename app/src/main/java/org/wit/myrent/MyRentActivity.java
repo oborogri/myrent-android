3d1caaf6e34ebae168294bf19b90e8cea5607a2f
oborogri <grigore.oboroceanu@gmail.com>
2016-09-26 14:34:57 +0100

    MyRent-00(step09-10): Residence model and listener added

--- app/src/main/java/org/wit/myrent/MyRentActivity.java
+++ app/src/main/java/org/wit/myrent/MyRentActivity.java
@@ -2,15 +2,26 @@ package org.wit.myrent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.text.Editable;
 
-public class MyRentActivity extends AppCompatActivity {
+public class MyRentActivity extends AppCompatActivity implements TextWatcher {
+    private EditText geolocation;
+    private Residence residence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myrent);
+
+        geolocation = (EditText) findViewById(R.id.geolocation);
+        residence = new Residence();
+
+        // Register a TextWatcher in the EditText geolocation object
+        geolocation.addTextChangedListener(this);
     }
 
     @Override
@@ -32,5 +43,21 @@ public class MyRentActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable)
+    {
+        residence.setGeolocation(editable.toString());
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence c, int start, int count, int after)
+    {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence c, int start, int count, int after)
+    {
     }
 }
