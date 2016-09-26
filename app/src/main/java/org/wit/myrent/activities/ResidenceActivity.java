6013e0b1155209d87e90097c2b358e5d45f9877a
oborogri <grigore.oboroceanu@gmail.com>
2016-09-26 16:17:51 +0100

    MyRent-01(step-05):Residence model updated with date and rented checkbox widgets

--- app/src/main/java/org/wit/myrent/activities/ResidenceActivity.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceActivity.java
@@ -3,17 +3,23 @@ package org.wit.myrent.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.text.Editable;
 
 import org.wit.myrent.R;
 import org.wit.myrent.models.Residence;
 
-public class ResidenceActivity extends AppCompatActivity implements TextWatcher {
+public class ResidenceActivity extends AppCompatActivity implements TextWatcher, CompoundButton.OnCheckedChangeListener {
     private EditText geolocation;
     private Residence residence;
+    private CheckBox rented;
+    private Button dateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,12 @@ public class ResidenceActivity extends AppCompatActivity implements TextWatcher 
 
         // Register a TextWatcher in the EditText geolocation object
         geolocation.addTextChangedListener(this);
+
+        dateButton  = (Button)   findViewById(R.id.registration_date);
+        rented  = (CheckBox) findViewById(R.id.isrented);
+        rented.setOnCheckedChangeListener(this);
+
+        dateButton .setEnabled(false);
     }
 
     @Override
@@ -62,5 +74,12 @@ public class ResidenceActivity extends AppCompatActivity implements TextWatcher 
     @Override
     public void onTextChanged(CharSequence c, int start, int count, int after)
     {
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton arg0, boolean isChecked)
+    {
+        Log.i(this.getClass().getSimpleName(), "rented Checked");
+        residence.rented = isChecked;
     }
 }
