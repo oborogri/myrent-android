20830c3b4842606f85ff00be640e42d2e8825288
oborogri <grigore.oboroceanu@gmail.com>
2016-09-30 11:19:28 +0100

    MyRent_v_05(complete):Up button in residence_activity implemented

--- app/src/main/java/org/wit/myrent/activities/ResidenceActivity.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceActivity.java
@@ -1,18 +1,18 @@
+
+
 package org.wit.myrent.activities;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.text.Editable;
 
 import org.wit.myrent.R;
 import org.wit.myrent.app.MyRentApp;
@@ -22,23 +22,32 @@ import org.wit.myrent.models.Residence;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-
 import android.app.DatePickerDialog;
 import android.view.View;
-import android.view.View.OnClickListener;
+import static org.wit.myrent.helpers.IntentHelper.navigateUp;
 
-public class ResidenceActivity extends Activity implements TextWatcher, CompoundButton.OnCheckedChangeListener, View.OnClickListener, DatePickerDialog.OnDateSetListener
+
+
+public class ResidenceActivity extends AppCompatActivity implements TextWatcher,
+        CompoundButton.OnCheckedChangeListener,
+        View.OnClickListener,
+        DatePickerDialog.OnDateSetListener
+
+
 {
     private EditText geolocation;
     private Residence residence;
+
     private CheckBox rented;
     private Button dateButton;
+
     private Portfolio portfolio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_residence);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         geolocation = (EditText) findViewById(R.id.geolocation);
         residence = new Residence();
@@ -46,54 +55,50 @@ public class ResidenceActivity extends Activity implements TextWatcher, Compound
         // Register a TextWatcher in the EditText geolocation object
         geolocation.addTextChangedListener(this);
 
-        dateButton  = (Button)   findViewById(R.id.registration_date);
+        dateButton = (Button) findViewById(R.id.registration_date);
         dateButton  .setOnClickListener(this);
 
-        rented  = (CheckBox) findViewById(R.id.isrented);
+        rented = (CheckBox) findViewById(R.id.isrented);
 
         MyRentApp app = (MyRentApp) getApplication();
         portfolio = app.portfolio;
-
         Long resId = (Long) getIntent().getExtras().getSerializable("RESIDENCE_ID");
         residence = portfolio.getResidence(resId);
-
-        if (residence != null)
-        {
+        if (residence != null) {
             updateControls(residence);
         }
     }
 
-    public void updateControls(Residence residence)
-    {
+    public void updateControls(Residence residence) {
         geolocation.setText(residence.geolocation);
         rented.setOnCheckedChangeListener(this);
+
         dateButton.setText(residence.getDateString());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //inflate the menu; this adds item to the action bar if it is present
-        getMenuInflater().inflate(R.menu.myrent, menu);
-        return true;
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        //handle action bar items clicks here. The action bar will
-        //automatically handle clicks on the Home/Up button, so long
-        //as you specify a parent activity in AndroidManifest.xml
-        int id = item.getItemId();
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
-    public void onClick(View view)
-    {
+    public void afterTextChanged(Editable editable) {
+        residence.setGeolocation(editable.toString());
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        Log.i(this.getClass().getSimpleName(), "rented Checked");
+        residence.rented = isChecked;
+    }
+
+    @Override
+    public void onClick(View view) {
         switch (view.getId())
         {
             case R.id.registration_date      : Calendar c = Calendar.getInstance();
@@ -104,8 +109,7 @@ public class ResidenceActivity extends Activity implements TextWatcher, Compound
     }
 
     @Override
-    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
-    {
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
         Date date = new GregorianCalendar(year, monthOfYear, dayOfMonth).getTime();
         residence.date = date.getTime();
         dateButton.setText(residence.getDateString());
@@ -119,26 +123,14 @@ public class ResidenceActivity extends Activity implements TextWatcher, Compound
     }
 
     @Override
-    public void afterTextChanged(Editable editable)
+    public boolean onOptionsItemSelected(MenuItem item)
     {
-        residence.setGeolocation(editable.toString());
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence c, int start, int count, int after)
-    {
-    }
-
-    @Override
-    public void onTextChanged(CharSequence c, int start, int count, int after)
-    {
-    }
-
-    @Override
-    public void onCheckedChanged(CompoundButton arg0, boolean isChecked)
-    {
-        Log.i(this.getClass().getSimpleName(), "rented Checked");
-        residence.rented = isChecked;
+        switch (item.getItemId())
+        {
+            case android.R.id.home:  navigateUp(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/org/wit/myrent/helpers/IntentHelper.java
+++ app/src/main/java/org/wit/myrent/helpers/IntentHelper.java
@@ -3,7 +3,7 @@ import java.io.Serializable;
 
 import android.app.Activity;
 import android.content.Intent;
-
+import android.support.v4.app.NavUtils;
 public class IntentHelper
 {
     public static void startActivity (Activity parent, Class classname)
@@ -24,5 +24,11 @@ public class IntentHelper
         Intent intent = new Intent(parent, classname);
         intent.putExtra(extraID, extraData);
         parent.startActivityForResult(intent, idForResult);
+    }
+
+    public static void navigateUp(Activity parent)
+    {
+        Intent upIntent = NavUtils.getParentActivityIntent(parent);
+        NavUtils.navigateUpTo(parent, upIntent);
     }
 }
